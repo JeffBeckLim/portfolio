a06546e0001819bbb93b7fc16d8077f75b2bc727
Jeff Beck M. Lim <101444919+JeffBeckLim@users.noreply.github.com>
2024-02-11 22:43:28 +0800

    waves

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import './App.css';
 
 import Hero from './components/Hero'
 import NavBar from './components/NavBar';
-
+import Card from './components/Card';
 function App() {
   return (
     <div>
-      <div className='top-wave'></div>
-      <NavBar />  
+      <div className='top-wave animate-top-bot'></div>
+
+      <NavBar />
       <Hero />
+      <Card />
     </div>
   );
 }
-
+window.addEventListener('load', function() {
+    const elements = document.querySelectorAll('.top-wave');
+    elements.forEach(element => {
+      element.classList.add('animate'); // Add a class to trigger the animation
+    });
+});
 export default App;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,46 +10,58 @@ import reactLogo from '../assets/react.png';
 import pgLogo from '../assets/pg.png';
 const Hero = () => {
   return (
-    <div className="main-container">
-        <div className="main main__profile">
-            <img src={ProfileImage} alt=""/>
+    <div className='hero-section'>
+        <div className="main-container">
+            <div className="main main__profile">
+                <img className='shadow' src={ProfileImage} alt=""/>
+            </div>
+
+            <div className="main main__content">
+                <h2>Web Developer <br /> 
+                    and Digital Artist 
+                    <img src={Pencil} alt="" style={{height: '36px', padding: '0px 0px 0px 10px'}} />
+                </h2>
+                <h6>
+                    Hello, I'm Jeff Beck M. Lim. A passionate Web Developer <br /> and Digital Artist Based in the Philippines
+                    🇵🇭
+                </h6>
+
+                <span>
+                    <i className="fa-brands fa-github"></i>
+                </span>   
+                <span>
+                    <i className="fa-brands fa-linkedin"></i>
+                </span>
+                <span>
+                    <i className="fa-brands fa-artstation"></i>
+                </span>
+            </div>
+
+            <div className='main main__techStack'>
+                <h2>Tech Stack </h2>
+                <span>
+                    <img src={htmLogo} alt="" />
+                    <img src={cssLogo} alt="" />
+                    <img src={jsLogo} alt="" />
+                    <img src={bsLogo} alt="" />
+                    <img src={laravelLogo} alt="" />
+                    <img src={reactLogo} alt="" />
+                    <img src={pgLogo} alt="" />
+                </span>
+            </div>
+        </div>
+        
+
+
+        <div class="hero-bottom-wave animate-bot-top">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+            </svg>
         </div>
 
-        <div className="main main__content">
-            <h2>Web Developer <br /> 
-                and Digital Artist 
-                <img src={Pencil} alt="" style={{height: '36px', padding: '0px 0px 0px 10px'}} />
-            </h2>
-            <h6>
-                Hello, I'm Jeff Beck M. Lim. A passionate Web Developer <br /> and Digital Artist Based in the Philippines
-                🇵🇭
-            </h6>
 
-            <span>
-                <i className="fa-brands fa-github"></i>
-            </span>   
-            <span>
-                <i className="fa-brands fa-linkedin"></i>
-            </span>
-            <span>
-                <i className="fa-brands fa-artstation"></i>
-            </span>
-        </div>
 
-        <div className='main main__techStack'>
-            <h2>Tech Stack </h2>
-            <span>
-                <img src={htmLogo} alt="" />
-                <img src={cssLogo} alt="" />
-                <img src={jsLogo} alt="" />
-                <img src={bsLogo} alt="" />
-                <img src={laravelLogo} alt="" />
-                <img src={reactLogo} alt="" />
-                <img src={pgLogo} alt="" />
-            </span>
-        </div>
    </div>
   )
 }
-
 export default Hero
